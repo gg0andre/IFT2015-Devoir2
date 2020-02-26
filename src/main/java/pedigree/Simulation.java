ce230c5e8fff97b8db55dbe515a4a7c96ecc9e5f
Andre Tran <andreviet2009@hotmail.com>
2020-02-25 19:32:33 -0500

    simulation done

--- src/main/java/pedigree/Simulation.java
+++ src/main/java/pedigree/Simulation.java
@@ -3,140 +3,130 @@ package pedigree;
 import java.util.PriorityQueue;
 import java.util.Random;
 
-import static pedigree.Event.eventType.Death;
-
 public class Simulation {
-
-    private static final double FIDELITE = 0.9;
-    private AgeModel ageModel = new AgeModel();
-    private Population population = new Population();               //Stockage de la population
-    private PriorityQueue<Event> eventQ = new PriorityQueue<Event>();   //Stockage des events
+    private AgeModel ageModel;
+    private Population population;
+    private PriorityQueue<Event> eventQ;
     private Random RND = new Random();
-    private double currentTime;
-    private double birthRate;
+
+    public double birthRate;
+    private static final double FIDELITY = 0.9;
 
     public void simulate(int n, double Tmax) {
-        Sim fondateur;
+        this.eventQ = new PriorityQueue<Event>();    // file de priorité
+        this.population = new Population();          //notre population
+        this.ageModel = new AgeModel();
+
+        double time = 0;
         birthRate = 2.0 / ageModel.expectedParenthoodSpan(Sim.MIN_MATING_AGE_F, Sim.MAX_MATING_AGE_F);
+        Sim foundateur;
 
-        for(int i=0; i<n; i++) {
-            if(RND.nextDouble() >= 0.5) {
-                fondateur = new Sim(Sim.Sex.M);
-            } else {
-                fondateur = new Sim(Sim.Sex.F);
-            }
-
+        for (int i = 0; i < n; i++) {
+            Sim fondateur = new Sim(chooseSex()); // sexe au hasard, naissance à 0.0
             Event E = new Event(fondateur, 0.0, Event.eventType.Birth);
-            eventQ.add(E);
+            eventQ.add(E); // insertion dans la file de priorité
         }
 
         while (!eventQ.isEmpty()) {
-            Event E = eventQ.poll();    //retirer le premier event
-            currentTime = 0.0;
+            Event E = eventQ.poll(); // prochain événement
 
-            if (E.time > Tmax) break;
+            if (E.time > Tmax) break; // arrêter à Tmax
 
-            if(E.time > currentTime+100) {
-                System.out.println("Temps : " + E.time);
-                System.out.println("Population : " + population.getSize());
-                currentTime = E.time;
-            }
-
-            if (E.sim.getDeathTime() > E.time) {
+            if (E != null && E.sim.getDeathTime() > E.time) {
+                System.out.println("population : " + population.getSize() + "\tat time : " + E.time);
                 switch (E.type) {
                     case Birth:
-                        //duree de vie du nouveau sim
-                        double deathTime= ageModel.randomAge(RND) + E.time;
-
-                        //attente de reproduction
                         if (E.sim.getSex() == Sim.Sex.F) {
-                            double startReproductionTime = Sim.MIN_MATING_AGE_F + E.time + AgeModel.randomWaitingTime(RND, birthRate);
-                            Event reproduction = new Event(E.sim, startReproductionTime, Event.eventType.Reproduction);
-                            eventQ.add(reproduction);
+                            double reproductionTime = Sim.MIN_MATING_AGE_F + E.time +
+                                    ageModel.randomWaitingTime(RND, birthRate);
+                            Event reproductionEvent = new Event(E.sim, 0.0, Event.eventType.Reproduction);
+                            eventQ.add(reproductionEvent);
                         }
 
-                        E.sim.setDeathTime(deathTime);
-                        Event death = new Event(E.sim, deathTime, Death);
+                        //duree de vie du sim + temp de naissance
+                        double deathTime = ageModel.randomAge(RND) + E.time;
+                        E.sim.setDeathTime(deathTime);  //Ajouter au sim son deathTime
+
+                        //creer le event et l'ajouter a la PQ
+                        Event death = new Event(E.sim, deathTime, Event.eventType.Death);
                         eventQ.add(death);
 
+                        //ajouter la liste de population
                         population.add(E.sim);
                         break;
 
                     case Reproduction:
-                        if (E.time > E.sim.getDeathTime()) break;
-
                         if (E.time - E.sim.getBirthTime() >= Sim.MIN_MATING_AGE_F) {
-                            Sim mate = chooseMate(E.sim, E.time);
-                            Sim child = reproduce(E.sim, mate, E.time);
+                            Sim mate = chooseMate(E.sim, E.time);                   //choisir le partenaire
+                            Sim child = new Sim(E.sim, mate, E.time, chooseSex());  //creer l'enfant
 
-                            Event birth = new Event(child, E.time, Event.eventType.Birth);
-                            eventQ.add(birth);
+                            //setMate pour parents
+                            E.sim.setMate(mate);
+                            mate.setMate(E.sim);
 
+                            //ajout a eventQ
+                            Event childBirth = new Event(child, E.time, Event.eventType.Birth);
+                            eventQ.add(childBirth);
                         }
 
                         double reproductionWaitTime = ageModel.randomWaitingTime(RND, birthRate);
-                        Event reproEvent = new Event(E.sim, E.time + reproductionWaitTime, Event.eventType.Reproduction);
-                        eventQ.add(reproEvent);
+                        Event reproductionEvent = new Event(E.sim, E.time + reproductionWaitTime,
+                                Event.eventType.Reproduction);
+                        eventQ.add(reproductionEvent);
 
                         break;
 
-                    case Death :
-                        if (population.getSize()==0) throw new IllegalStateException("pas de population");
+                    case Death:
                         population.deleteMin();
                         break;
                 }
-            }
-            currentTime+=5;
+
+            } // else rien à faire avec E car son sujet est mort
+            time++;
         }
     }
 
-    //Choisir un pere, si possible rester avec le precedent
-    public Sim chooseMate(Sim sim, double time) {
-        Sim newMate = null;
-
-        if(sim.isInARelationship(time) && RND.nextDouble() < FIDELITE) {
-            return sim.getMate();
+    //fonction pour choisir le sexe aleatoire
+    public Sim.Sex chooseSex() {
+        if(RND.nextInt() > 0.5) {
+            return Sim.Sex.M;
         } else {
-            //nouveau partenaire
-            Sim potentiel = population.randomSim();
-            do {
-                if (potentiel.getSex().equals(Sim.Sex.M) && potentiel.isMatingAge(time)) {
-                    if(!potentiel.isInARelationship(time) || RND.nextDouble() < FIDELITE) {
-                        newMate = potentiel;
-                    }
-                }
-
-            } while (newMate == null) ;
+            return Sim.Sex.F;
         }
-
-        return newMate;
     }
 
-    //Naissance d'un nouveau sim de 2 sims
-    public Sim reproduce(Sim mere, Sim pere, double time) {
-        Sim.Sex sex = Math.random() < 0.5? Sim.Sex.M : Sim.Sex.F;
-        Sim child = new Sim(mere, pere, time, sex);
-
-        //ajouter le lien entre pere et mere
-        mere.setMate(pere);
-        pere.setMate(mere);
-
-        return child;
+    //fonction pour choisir un (nouveau) partenaire pour la mere
+    public Sim chooseMate(Sim x, double time) {
+        Sim y = null;
+        Sim z;
+        if(!x.isInARelationship(time) || RND.nextDouble() > FIDELITY) {
+            do {
+                z = population.randomSim();
+                // isMatingAge() vérifie si z est de l'age adéquat
+                if (z.getSex() != x.getSex() && z.isMatingAge(time)) {
+                    if (x.isInARelationship(time)   // z accepte si x est infidèle
+                        || !z.isInARelationship(time)
+                        || RND.nextDouble() > FIDELITY)
+                    {   y = z; }
+                }
+            } while (y == null);
+        } else { y = x.getMate(); }
+        return y;
     }
-
 
     public static void main(String[] args) {
-        int n;
-        int Tmax;
+        int n; int Tmax;
+
         if (args.length != 2) {
             System.out.println("Invalid number of arguments");
         } else {
-            n = 5;
-            Tmax = 150;
-            Simulation test = new Simulation();
+            n = Integer.parseInt(args[0]);
+            Tmax = Integer.parseInt(args[1]);
 
+            Simulation test = new Simulation();
             test.simulate(n, Tmax);
-            System.out.println("stop");
         }
+
     }
+
 }
